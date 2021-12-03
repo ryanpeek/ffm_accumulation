bdb271bbc17060efc0b254fa352900be9e29d2ca
Ryan Peek <rapeek@ucdavis.edu>
2021-12-03 00:19:15 -0800

    upate

--- R/download_scibase.R
+++ R/download_scibase.R
@@ -1,7 +1,14 @@
 # ScienceBase tools
 # tutorial here: https://owi.usgs.gov/R/training-curriculum/usgs-packages/sbtools-discovery/
 library(sbtools)
-
+library(purrr)
+library(glue)
+library(fs)
+library(xml2)
+library(tidyr)
+library(tibble)
+library(dplyr, warn.conflicts = FALSE)
+options(scipen = 100)
 
 # Query ScienceBase based on catchment keyword
 #query_sb_text('Catchment Values', limit=1)
@@ -28,16 +35,41 @@ length(children)
 # list all files
 all_files <- item_list_files(id_item,recursive = TRUE)
 dim(all_files) # 1412 files!
+# add sizes
+all_files <- all_files %>%
+  mutate(size_mb = size * 1e-6, .after=size)
+
 main_files <- item_list_files(id_item)
-main_files
 
 # get it?
 (id_item_get <- item_get(id_item))
 names(id_item_get)
 
-# test download
+# clean dir
+fs::dir_delete("data_raw/scibase_flow/")
+# create dir
 fs::dir_create("data_raw/scibase_flow")
 
-library(purrr)
-library(glue)
-map2(all_files$url[6:10], all_files$fname[6:10], ~download.file(.x, glue("data_raw/scibase_flow/{.y}")))
+# download
+map2(all_files$url[7:nrow(all_files)], all_files$fname[7:nrow(all_files)], ~download.file(.x, glue("data_raw/scibase_flow/{.y}")))
+
+
+# get only xml files
+xml_ls <- dir_ls("data_raw/scibase_flow/", type = "file", glob = "*.xml")
+
+# read one in and parse?
+xml1 <- as_list(xml2::read_xml(xml_ls[2]))
+
+# make long
+(xml_df <- as_tibble(xml1) %>% unnest_longer(metadata))
+unique(xml_df$metadata_id)
+
+xml_df_wide <- xml_df %>%
+  filter(DATA_ID=="LP")
+xml2::xml_attrs(xml1, "metadata")
+
+# see here?
+# https://urbandatapalette.com/post/2021-03-xml-dataframe-r/
+
+# look at fields
+item_get_fields(id_item, c("summary", "tags"))
